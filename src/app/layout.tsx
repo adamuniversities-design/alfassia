809ae11240c9a2006cc1ad286e6ec20fa14e5eec
adamuniversities-design <adamuniversities@gmail.com>
2026-03-02 11:29:44 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,18 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children as any;
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Alfassia Allhurra',
+  description: 'Soins de luxe artisanaux, nés à Fès, Maroc.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
 }
